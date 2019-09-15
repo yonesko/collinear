99127b05696d35c963d07541d1f2692180ea8d03
glebio <yonesko@gmail.com>
2019-09-15 19:48:35 +0300

    BruteCollinearPoints

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -5,6 +5,19 @@
  **************************************************************************** */
 
 public class BruteCollinearPoints {
+
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    public int numberOfSegments() {
+        return 1;
+    }
+
+    public LineSegment[] segments() {
+        return null;
+    }
+
     public static void main(String[] args) {
 
     }
